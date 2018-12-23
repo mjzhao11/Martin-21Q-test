7899b0945e24daf422ae7a8c6ebcfc30c3a522de
Martin Zhao <mjzhao11@ymail.com>
2018-12-23 01:25:30 -0800

    Initial Commit

---
+++ Martin 21Q testTests/Martin_21Q_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  Martin_21Q_testTests.swift
+//  Martin 21Q testTests
+//
+//  Created by Martin Zhao on 2018-12-23.
+//  Copyright © 2018 Martin Zhao. All rights reserved.
+//
+
+import XCTest
+@testable import Martin_21Q_test
+
+class Martin_21Q_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
